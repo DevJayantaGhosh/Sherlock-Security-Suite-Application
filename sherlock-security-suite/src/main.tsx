2014b771b628729b4f1fe95aaeea0078be6913f6
DevJayantaGhosh <jayantaghoshnitdgp20@gmail.com>
2025-12-01 04:45:53 +0530

    toaster included

--- sherlock-security-suite/src/main.tsx
+++ sherlock-security-suite/src/main.tsx
@@ -5,12 +5,23 @@ import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from './theme';
 import './index.css';
+import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <App />
+      <Toaster
+        position="bottom-right"
+        toastOptions={{
+          style: {
+            background: "#0b0f20",
+            color: "#fff",
+            border: "1px solid rgba(255,255,255,0.1)"
+          }
+        }}
+      />
     </ThemeProvider>
   </React.StrictMode>
 );
